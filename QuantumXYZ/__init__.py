eac38466d8ee1c995a3c113fab7375d69e075a42
nozidoali <nozidoali@gmail.com>
2023-06-28 14:41:27 +0200

    try to fix:

--- QuantumXYZ/__init__.py
+++ QuantumXYZ/__init__.py
@@ -5,9 +5,11 @@
 Author: Hanyu Wang
 Created time: 2023-06-28 14:34:30
 Last Modified by: Hanyu Wang
-Last Modified time: 2023-06-28 14:34:45
+Last Modified time: 2023-06-28 14:40:59
 '''
 
 from .Algorithms import *
 from .Circuit import *
 from .StatePreparator import *
+from .Utils import *
+from .Visualization import *
