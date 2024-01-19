fix rotation angle of exact cnot synthesis

tests/functional_tests/test_synthesis.py
@@ -12,6 +12,7 @@ import random
 from itertools import combinations
 
 import numpy as np
+from pyrsistent import v
 import pytest
 
 from xyz import QState, quantize_state
@@ -75,8 +76,9 @@ def state_vectors():
     """Generate a random state vector for testing ."""
     all_state_vectors = []
     while len(all_state_vectors) < 1:
-        num_qubit = random.randint(3, 5)
+        num_qubit = random.randint(3, 3)
         sparsity = random.randint(num_qubit, 2 ** (num_qubit - 1) - 1)
+        sparsity = random.randint(2 ** (num_qubit - 1) - 1, 2 ** (num_qubit - 1) - 1)
         state = rand_state(num_qubit, sparsity, uniform=False)
 
         # check if the state is valid
@@ -95,27 +97,28 @@ def test_one_state(state_vectors):
     for state_vector in state_vectors:
         state_vector_exp = state_vector
         target_state = quantize_state(state_vector_exp)
-        print("target state: ", target_state)
+        # print("target state: ", target_state)
 
-        circuit = hybrid_cnot_synthesis(target_state)
+        circuit = hybrid_cnot_synthesis(target_state, verbose_level=3)
 
         # now we measure the distance between the target state and the actual state
         state_vector_act = simulate_circuit(circuit).data
         dist = np.linalg.norm(state_vector_act - state_vector_exp)
 
-        # print("target state: ", target_state)
-        # print("actual state: ", quantize_state(state_vector_act))
+        print("target state: ", target_state)
+        print("actual state: ", quantize_state(state_vector_act))
 
-        # circ = circuit.to_qiskit()
-        # print(circ)
+        circ = circuit.to_qiskit()
+        print(circ)
 
         assert dist < 1e-1
 
 if __name__ == "__main__":
     rand_state_vectors = []
     for i in range(100):
-        num_qubit = random.randint(3, 4)
+        num_qubit = random.randint(3, 3)
         sparsity = random.randint(num_qubit, 2 ** (num_qubit - 1) - 1)
+        sparsity = random.randint(2 ** (num_qubit - 1) - 1, 2 ** (num_qubit - 1) - 1)
         state = rand_state(num_qubit, sparsity, uniform=False)
         rand_state_vectors.append(state)
         

xyz/algorithms/synthesis/hybrid_synthesis.py
@@ -83,6 +83,7 @@ class HybridCnotSynthesisStatistics:
 def _hybrid_cnot_synthesis_impl(
     circuit: QCircuit,
     state: QState,
+    verbose_level: int = 0,
     stats: HybridCnotSynthesisStatistics = None,
 ):
     prev_supports = state.get_supports()
@@ -160,7 +161,7 @@ def _hybrid_cnot_synthesis_impl(
                     circuit,
                     state,
                     optimality_level=3,
-                    verbose_level=0,
+                    verbose_level=verbose_level,
                     cnot_limit=EXACT_SYNTHESIS_CNOT_LIMIT,
                 )
             if stats is not None:
@@ -249,7 +250,7 @@ def _hybrid_cnot_synthesis_impl(
         neg_state, pos_state, weights0, weights1 = state.cofactors(pivot)
 
         # we first add a rotation gate to the pivot qubit
-        theta = 2 * np.arccos(np.sqrt(weights0 / (weights0 + weights1)))
+        theta = 2 * np.arctan(weights1 / weights0)
         ry_gate = RY(theta, pivot_qubit)
 
         pos_gates = _hybrid_cnot_synthesis_impl(
@@ -318,6 +319,7 @@ def _hybrid_cnot_synthesis_impl(
 def hybrid_cnot_synthesis(
     state: QState,
     map_gates: bool = True,
+    verbose_level: int = 0,
     stats: HybridCnotSynthesisStatistics = None,
 ):
     """A hybrid method combining both qubit- and cardinality- reduction.
@@ -358,6 +360,7 @@ def hybrid_cnot_synthesis(
         gates, _ = _hybrid_cnot_synthesis_impl(
             circuit,
             state,
+            verbose_level=verbose_level,
             stats=stats,
         )
 

xyz/operator/ctr.py
@@ -8,6 +8,8 @@ Last Modified by: Hanyu Wang
 Last Modified time: 2023-08-27 01:10:43
 """
 
+import numpy as np
+
 from typing import Any
 
 from xyz.qstate import QState
@@ -70,7 +72,7 @@ class CTROperator(QOperatorBase, QuantizedRotation, ControlledOperator):
             )
 
             # this is an interesting case
-            self.theta = theta
+            self.theta = theta - np.pi
             return next_state
 
     def __str__(self) -> str:

xyz/operator/tr.py
@@ -8,6 +8,8 @@ Last Modified by: Hanyu Wang
 Last Modified time: 2023-08-27 00:39:26
 """
 
+import numpy as np
+
 from typing import Any
 from xyz.qstate import QState
 
@@ -52,7 +54,7 @@ class TROperator(QOperatorBase, QuantizedRotation):
         if self.operator_type == QOperatorType.T1:
             next_state, theta = qstate.apply_merge1(self.target_qubit_index)
             # this is an interesting case, we need to rotate to the opposite direction
-            self.theta = theta
+            self.theta = theta - np.pi # we need to rotate another pi
             return next_state
 
     def __str__(self) -> str:

xyz/qstate/qstate.py
@@ -147,13 +147,13 @@ class QState:
             idx1 = idx0 ^ (1 << qubit_index)
 
             # now we check the rotation angle
-            weight_from = self.index_to_weight[idx1]
-            weight_to = self.index_to_weight[idx0]
+            weight0 = self.index_to_weight[idx0]
+            weight1 = self.index_to_weight[idx1]
             weight_total = np.sqrt(
-                (self.index_to_weight[idx1] ** 2) + (self.index_to_weight[idx0] ** 2)
+                (weight0 ** 2) + (weight1 ** 2)
             )
 
-            _theta = 2 * np.arctan(weight_to / weight_from)
+            _theta = 2 * np.arctan(weight0 / weight1)
 
             if theta is None:
                 theta = _theta
@@ -248,13 +248,13 @@ class QState:
             idx1 = idx0 ^ (1 << target_qubit_index)
 
             # now we check the rotation angle
-            weight_from = self.index_to_weight[idx1]
-            weight_to = self.index_to_weight[idx0]
+            weight0 = self.index_to_weight[idx0]
+            weight1 = self.index_to_weight[idx1]
             weight_total = np.sqrt(
-                (self.index_to_weight[idx1] ** 2) + (self.index_to_weight[idx0] ** 2)
+                (weight0 ** 2) + (weight1 ** 2)
             )
 
-            _theta = 2 * np.arctan(weight_to / weight_from)
+            _theta = 2 * np.arctan(weight0 / weight1)
 
             if theta is None:
                 theta = _theta