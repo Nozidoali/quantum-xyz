77d1c1051a44809cbcaae7ef5c5ce445e10bbb5e
hanyu <nozidoali@gmail.com>
2024-03-16 15:51:47 +0100

    use version placeholder

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="quantum-xyz",
-    version="0.1.0",
+    version="{{VERSION_PLACEHOLDER}}",
     author="Hanyu Wang",
     description="A quantum circuit synthesis library",
     classifiers=[
