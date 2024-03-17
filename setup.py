3a0547146a81900aabc1d741f66818740e53eb2d
hanyu <nozidoali@gmail.com>
2024-03-17 17:48:21 +0100

    sperate the deploy workflow

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="quantum-xyz",
-    # version="{{VERSION_PLACEHOLDER}}",
+    version="0.1.0",
     author="Hanyu Wang",
     description="A quantum circuit synthesis library",
     classifiers=[
